9ce9d82e30c5e3c8b722905ae4a71e41870b9fca
yanfeng12 <32252305+yanfeng12@users.noreply.github.com>
2018-08-01 15:30:43 +0800

    内置对象

---
+++ project/js/index.js
@@ -0,0 +1,10 @@
+//var oNavA = document.querySelectorAll(".nav")
+//console.log(oNavA)
+//for (var i=0,len = oNavA.length;i<len;i++) {
+//	oNavA[i].onmouseover = function  () {
+//		this.style.borderBottom = "5px solid #d90000"
+//	}
+//	oNavA[i].onmouseout = function  () {
+//		this.style.borderBottom = "1px solid white"
+//	}
+//}
